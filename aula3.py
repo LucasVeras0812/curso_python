f21c10b2e751376c1d7eda949aff77056c01be46
Lucas da Silva Veras <lucassv@cpack.local>
2025-01-24 13:52:50 -0300

    Aula 3 modificada e $ adicionada

--- aula3.py
+++ aula3.py
@@ -1,1 +1,20 @@
-print("Isto é um test!")
+"""
+Python = linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+#Aspas simples
+print('Lucas')
+print('Lucas "Veras"')
+
+#Aspas duplas
+print("Lucas")
+
+#Escape
+print("Lucas \"Veras\"")
+
+# r
+print(r"Lucas \"Veras\"")
+
