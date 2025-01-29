f05abf988c6b26a4ef6aaca921bbd5d01312afc7
Lucas da Silva Veras <lucassv@cpack.local>
2025-01-29 13:19:40 -0300

    Aulas 20 e 21 modificadas/22 a 25 adicionadas

---
+++ aula23.py
@@ -0,0 +1,12 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input("Senha: ")
+
+# if not senha:
+#     print("Voce nao digitou nada!")
+
+print(not False) #True
+print(not True) #False
