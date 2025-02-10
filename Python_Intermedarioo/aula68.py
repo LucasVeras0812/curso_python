8403520333fd3286b9bef3e227a6c7c5981f45ac
Lucas <cris.matrix02@gmail.com>
2025-02-10 16:22:01 -0300

    Python Intermediario

---
+++ Python_Intermedarioo/aula68.py
@@ -0,0 +1,9 @@
+"""
+Escopo de funçoes em Python
+Escopo significa o local onde aquele codigo pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel
+O escolo local é o escopo onde apenas nomes do mesmo local
+pode ser alcançados.
+"""
+
