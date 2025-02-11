41b4d6194f6f27de091d6ba073dbf464f3c8436e
Lucas da Silva Veras <lucassv@cpack.local>
2025-02-11 13:14:39 -0300

    Python Intermediario

---
+++ Python_Intermedarioo/aula70.py
@@ -0,0 +1,14 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x,y):
+    return x + y
+
+# variavel = soma(1,3)
+# variavel = int("1")
+
+soma1 = soma(2,2)
+soma2 = soma(3,4)
+
+print(soma1 + soma2)
