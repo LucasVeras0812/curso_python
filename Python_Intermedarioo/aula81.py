8a24758958a17b7770a115666d7d648d8d9ad933
Lucas da Silva Veras <lucassv@cpack.local>
2025-02-17 13:07:52 -0300

    Python Intermediario

---
+++ Python_Intermedarioo/aula81.py
@@ -0,0 +1,26 @@
+# Função lambda em python
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anonimas
+# que contem apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única expressao.
+
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'Miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+] 
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
+
---
+++ Python_Intermedarioo/aula83.py
@@ -0,0 +1,9 @@
+a, b = 1, 2
+a, b = b, a
+
+print(a, b)
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
