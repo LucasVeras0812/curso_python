1b3b771c01604cfdaa63f7cf7c0724d81e9c79df
Lucas da Silva Veras <lucassv@cpack.local>
2025-02-25 13:23:27 -0300

    Python Intermediario & requirements

---
+++ Python_Intermedarioo/aula115.py
@@ -0,0 +1,12 @@
+# Ambientes virtuais em python (venv)
+# um ambiente virtual carrega toda a sua instalação
+# do python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do 
+# ambiente virtual será usada.
+# venve é o mmódulo que vamos usar para
+# criar ambientes virtuais.
+# Voce pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env, .venv .env
+
+print("oi")
