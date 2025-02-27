85e83c2dc7c8baaab7081c4de71565c08cfb55b0
Lucas da Silva Veras <lucassv@cpack.local>
2025-02-27 14:29:17 -0300

    Python avançado

--- Python_Avancado/aula130.py
+++ Python_Avancado/aula130.py
@@ -3,4 +3,32 @@
 # @classmethod - cls, método de classe
 # @staticmethod - método estático (❌self, ❌cls)
 
-print('❌')
+class Connection:
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+
+    def set_user(self, user):
+        self.user = user
+
+    def set_password(self, password):
+        self.password = password
+
+    @classmethod
+    def create_with_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+    
+    @staticmethod
+    def log(msg):
+        print("Log:", msg)
+
+c1 = Connection.create_with_auth('luiz', '123')
+# c1.set_user('luiz')
+# c1.set_password('123')
+print(c1.user)
+print(c1.password)
+print(Connection.log("Mensagem log"))
