c204a02bb23ec0b244d74e69129751a35892cd0e
Lucas <cris.matrix02@gmail.com>
2025-03-27 13:22:27 -0300

    Python Avançado Módulos

--- Python_Avancado_Models/aula162.py
+++ Python_Avancado_Models/aula162.py
@@ -1,4 +1,4 @@
-  # Criando datas com módulo datetime
+# Criando datas com módulo datetime
 # datetime(ano, mês, dia)
 # datetime(ano, mês, dia, horas, minutos, segundos)
 # datetime.strptime('DATA', 'FORMATO')
@@ -12,10 +12,14 @@
 # pip install pytz types-pytz
 from datetime import datetime
 
-data_str_data = '2022/04/20 07:49:23'
-data_str_data = '20/04/2022'
-data_str_fmt = '%d/%m/%Y'
+# from pytz import timezone
 
-# data = datetime(2022, 4, 20, 7, 49, 23)
-data = datetime.strptime(data_str_data, data_str_fmt)
-print(data)
+data = datetime.now()
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1670849077))
+# data_str_data = '2022/04/20 07:49:23'
+# data_str_data = '20/04/2022'
+# data_str_fmt = '%d/%m/%Y'
+
+# data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
+# data = datetime.strptime(data_str_data, data_str_fmt)
