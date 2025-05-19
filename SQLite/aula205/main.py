2db2439edf2822bc87734d1ec00c945277163114
Lucas <cris.matrix02@gmail.com>
2025-05-19 10:24:22 -0300

    DBeaver aula

--- SQLite/aula205/main.py
+++ SQLite/aula205/main.py
@@ -1,16 +1,23 @@
 import sqlite3
 from pathlib import Path
 
-
-# SQLite - Aula 205
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-#SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
