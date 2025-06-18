ad9ada7a2a4c97b47ba27de9541e4694fd82dccc
Lucas <cris.matrix02@gmail.com>
2025-06-18 13:57:42 -0300

    .

--- Python_Basico/aula42.py
+++ Python_Basico/aula42.py
@@ -12,7 +12,7 @@ while i < len(frase):
     letra_atual = frase[i]
     quantas_vezes_letra_apareceu = frase.count(letra_atual)
 
-    if apareceu_mais_vezes <= letra_que_apareceu_mais_vezes:
+    if letra_que_apareceu_mais_vezes <= letra_que_apareceu_mais_vezes:
         apareceu_mais_vezes = letra_que_apareceu_mais_vezes
         apareceu_mais_vezes = quantas_vezes_letra_apareceu
 
